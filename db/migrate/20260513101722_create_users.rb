e5d16ef27fa4d61d60d42658fbf1d43b8725f130
cutalion <cutalion@gmail.com>
2026-05-15 20:38:14 +0400

    Squash unused migrations into create_users

--- db/migrate/20260513101722_create_users.rb
+++ db/migrate/20260513101722_create_users.rb
@@ -1,4 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
+  MAX_BALANCE = 1_000_000_000_000
+  EMAIL_MAX = 255
+
   def change
     enable_extension "citext" unless extension_enabled?("citext")
 
@@ -11,6 +14,11 @@ class CreateUsers < ActiveRecord::Migration[8.1]
 
     add_index :users, :email, unique: true
 
-    add_check_constraint :users, "balance >= 0", name: "users_balance_non_negative"
+    add_check_constraint :users, "balance >= 0",
+      name: "users_balance_non_negative"
+    add_check_constraint :users, "balance <= #{MAX_BALANCE}",
+      name: "users_balance_within_limit"
+    add_check_constraint :users, "char_length(email) <= #{EMAIL_MAX}",
+      name: "users_email_length"
   end
 end
