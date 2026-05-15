71a4f9e9e7c21dd34808cc5d9768e8034c162285
cutalion <cutalion@gmail.com>
2026-05-15 20:38:18 +0400

    Drop redundant LOWER() in Auth::IssueToken

--- app/services/auth/issue_token.rb
+++ app/services/auth/issue_token.rb
@@ -7,7 +7,7 @@ module Auth
     private
 
     def perform
-      user = User.find_by("LOWER(email) = ?", email.to_s.downcase)
+      user = User.find_by(email: email)
       fail!(:invalid_credentials, message: "Invalid credentials") unless user
 
       JsonWebToken.encode(sub: user.id, exp: 24.hours.from_now.to_i)
